4ed9d3fdf591124671a69d744dba9cdd7c5d7ffa
TeymurDev <teymur.ff@gmail.com>
2026-07-23 14:31:12 +0400

    feat: add shorter solution

--- reverse-string.js
+++ reverse-string.js
@@ -1,8 +1,8 @@
 function reverse(str) {
   let reversed = '';
 
-  for (let i = 0; i < str.length; i++) {
-    reversed = str[i] + reversed;
+  for (let char of str) {
+    reversed = char + reversed;
   }
 
   return reversed;
